5e9d3c3ecd114e43bf52a1f7ee9a48e3c8c63002
Ruanpbo <ruanpbo@hotmail.com>
2025-01-19 18:20:35 -0300

    Adicionando laço de repetição

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-alert("Bem vindo ao jogo");
-let numeroSecreto = 4;
-let chute = prompt("Escolha um numero de 1 a 10");
+let nome = prompt("Qual seu nome? ")
 
-if (chute == numeroSecreto){
-    alert(`Você acertou o numero secreto ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto){
-        alert(`O numero secreto é menor que ${chute}`)
+alert(`Bem vindo ao jogo ${nome}`);
+
+let numeroSecreto = 5;
+let chute;
+let tentativas =1;
+
+while (chute != numeroSecreto){
+
+    chute = prompt("Escolha um numero de 1 a 10");
+
+    if (chute == numeroSecreto){
+        alert(`Você acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
     } else {
-        alert(`O numero secreto é maior que ${chute}`)
+        if (chute > numeroSecreto){
+            alert(`O numero secreto é menor que ${chute}`);
+        } else {
+            alert(`O numero secreto é maior que ${chute}`);
+        }
+        tentativas++
     }
-} 
+}

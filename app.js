3c99148b3dd6391aae0039f6f502207e526a6ce0
Ruanpbo <ruanpbo@hotmail.com>
2025-01-19 12:42:35 -0300

    Adicionando variaveis e condicional

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Bem vindo ao jogo");
+let numeroSecreto = 4;
+let chute = prompt("Escolha um numero de 1 a 10");
+
+if (chute == numeroSecreto){
+    console.log("Você acertou o numero secreto (4)");
+}
